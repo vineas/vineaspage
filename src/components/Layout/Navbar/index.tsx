7799dbcbdeda639ed9aa42beed0bdf2540a07570
vineas <alvienasyandika@gmail.com>
2024-12-02 21:14:24 +0700

    edit footer

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -23,25 +23,29 @@ export const Navbar: React.FC = () => {
   return (
     <>
       <Disclosure>
-        <motion.div 
-        className="bg-gray-900 sticky z-50 top-0"
-        initial={{  opacity: 0 }}
-        whileInView={{  opacity: 1 }}
-        transition={{ duration: 1.5 }}>
+        <div className="bg-gray-900 sticky z-50 top-0">
           <div className="">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
               <div className="relative flex h-16 items-center justify-between">
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                  <div className="flex flex-shrink-0 items-center text-4xl md:text-4xl font-bold text-white">
+                  <motion.div 
+                  className="flex flex-shrink-0 items-center text-4xl md:text-4xl font-bold text-white"
+                  initial={{  opacity: 0 }}
+                  whileInView={{  opacity: 1 }}
+                  transition={{ duration: 1.5 }}>
                     <h2 className="VT323">
                       <a href="#">VineasPage</a>
                     </h2>
-                  </div>
+                  </motion.div>
                 </div>
 
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                   <div className="hidden sm:ml-6 sm:block">
-                    <div className="flex space-x-4">
+                    <motion.div 
+                    className="flex space-x-4"
+                    initial={{  opacity: 0 }}
+                    whileInView={{  opacity: 1 }}
+                    transition={{ duration: 1.5 }}>
                       {navigation.map((item) => (
                         <a
                           key={item.name}
@@ -52,7 +56,7 @@ export const Navbar: React.FC = () => {
                           {item.name}
                         </a>
                       ))}
-                    </div>
+                    </motion.div>
                   </div>
                 </div>
                 <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
@@ -89,7 +93,7 @@ export const Navbar: React.FC = () => {
               </div>
             </DisclosurePanel>
           </div>
-        </motion.div>
+        </div>
       </Disclosure>
     </>
   );
